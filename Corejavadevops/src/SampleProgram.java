8c6a9e4ea1a56adb1ad99a3119a66d9d43ce85dc
sirisha veluri <sirisha veluri@LAPTOP-EC8KGD28>
2021-09-23 10:52:57 +0530

    SampleProgram

---
+++ Corejavadevops/src/SampleProgram.java
@@ -0,0 +1,22 @@
+public class SampleProgram {
+	public static void main(String args[]) {
+		System.out.println("Hello world");
+		byte fn=3;
+		short roomn=705;
+		int eid=1020;
+		long mn=245678901;
+		float rating=5.0f;
+		double salary=55000.00;
+		char gender='m';
+		boolean maritalStatus=true;
+		System.out.println(fn);
+		System.out.println(roomn);
+		System.out.println(eid);
+		System.out.println(mn);
+		System.out.println(rating);
+		System.out.println(salary);
+		System.out.println(gender);
+		System.out.println(maritalStatus);
+	}
+
+}
---
+++ Corejavadevops/src/com/hcl/java/fundamentals/Operators.java
@@ -0,0 +1,24 @@
+package com.hcl.java.fundamentals;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		//declare and initialize values
+		int value1=10;
+		int value2=20;
+		//Arithmetic operators
+		int addResult=value1+value2;
+		int subResult=value1-value2;
+		int mulResult=value1*value2;
+		int divResult=value1/value2;
+		int modResult=value1%value2;
+		//display result
+		System.out.println(addResult);
+		System.out.println(subResult);
+		System.out.println(mulResult);
+		System.out.println(divResult);
+		System.out.println(modResult);
+
+	}
+
+}
